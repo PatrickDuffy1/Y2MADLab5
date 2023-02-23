c137b6059fbd21bcde7c6b613d06af6b958b29ff
G00408635@atu.ie <G00408635@atu.ie>
2023-02-23 15:12:43 +0000

    Added lab4

---
+++ src/app/componenta/componenta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componenta',
+  templateUrl: './componenta.component.html',
+  styleUrls: ['./componenta.component.css']
+})
+export class ComponentaComponent {
+
+}
